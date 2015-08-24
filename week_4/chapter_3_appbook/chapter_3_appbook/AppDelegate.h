e90b2117a87c68f142ac458a4efec973ced45faf
Michael Metzger <metzgerenator@gmail.com>
2015-08-24 14:24:01 -0400

    added chapter 3 folder and appcoda workalong

---
+++ week_4/chapter_3_appbook/chapter_3_appbook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  chapter_3_appbook
+//
+//  Created by Aileen Taboy on 8/24/15.
+//  Copyright (c) 2015 Mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ week_4/chapter_3_appbook/chapter_3_appbook/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  chapter_3_appbook
+//
+//  Created by Aileen Taboy on 8/24/15.
+//  Copyright (c) 2015 Mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+@end
+
+
+
+
