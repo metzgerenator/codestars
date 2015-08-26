6b7d72cc421860f09b63f74c94bf4f35891e9335
Michael Metzger <metzgerenator@gmail.com>
2015-08-26 17:55:07 -0400

    added draft of calculator app

---
+++ week_4/coding_excercise_2/calculator/calculator/ViewController.h
@@ -0,0 +1,20 @@
+//
+//  ViewController.h
+//  calculator
+//
+//  Created by Aileen Taboy on 8/26/15.
+//  Copyright (c) 2015 Mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (nonatomic, weak) IBOutlet UILabel *readout;
+
+
+
+-(IBAction)numberReader:(id)sender;
+
+
+@end
+
---
+++ week_4/coding_excercise_2/calculator/calculator/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  calculator
+//
+//  Created by Aileen Taboy on 8/26/15.
+//  Copyright (c) 2015 Mike. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+{
+    NSDictionary *numberKeys;
+}
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
