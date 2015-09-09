04cf8772ccff58d3fa04d46b43195e40cd394e81
Michael Metzger <metzgerenator@gmail.com>
2015-09-09 14:50:39 -0400

    added bio

---
+++ week_5/travelapp_3/travelapp_3/BioViewController.h
@@ -0,0 +1,15 @@
+
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BioViewController : UIViewController
+
+
+@property (nonatomic, strong)IBOutlet UIWebView *bioWeb;
+
+@property (nonatomic,strong)IBOutlet UIImageView *bioPic;
+@property (nonatomic, strong)UIImage *myImage;
+
+
+@end
---
+++ week_5/travelapp_3/travelapp_3/BioViewController.m
@@ -0,0 +1,46 @@
+//
+//  BioViewController.m
+//  travelapp_3
+//
+//  Created by Aileen Taboy on 9/9/15.
+//  Copyright (c) 2015 Mike. All rights reserved.
+//
+
+#import "BioViewController.h"
+
+@interface BioViewController ()
+
+@end
+
+@implementation BioViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    NSURL *url = [NSURL fileURLWithPath:[[NSBundle mainBundle]pathForResource:@"aboutme.html" ofType:nil]];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [self.bioWeb loadRequest:request];
+    
+    self.myImage = [UIImage imageNamed:@"mike.jpg"];
+
+    
+    self.bioPic.image = self.myImage;
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
