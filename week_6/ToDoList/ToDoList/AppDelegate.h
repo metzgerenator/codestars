d01d2e39f63d6122742ca423054882d4503a817c
metzgerenator <metzgerenator@gmail.com>
2015-09-10 14:25:38 -0400

    added work along for week 6

---
+++ week_6/ToDoList/ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Michael Metzger  on 9/10/15.
+//  Copyright (c) 2015 Michael Metzger . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
