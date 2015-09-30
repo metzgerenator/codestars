32c98cad6e44042255551047f5b2fb985e15aff9
metzgerenator <metzgerenator@gmail.com>
2015-09-30 19:57:28 -0400

    added album assignment

---
+++ week_9/myAlbums/AlbumInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  AlbumInfoViewController.h
+//  myAlbums
+//
+//  Created by Michael Metzger  on 9/30/15.
+//  Copyright © 2015 Michael Metzger . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AlbumInfoViewController : UIViewController
+
+@end
