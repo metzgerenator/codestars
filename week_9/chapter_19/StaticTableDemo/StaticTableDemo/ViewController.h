372de69c1ded92dd86153f07b44779949d05b4a9
metzgerenator <metzgerenator@gmail.com>
2015-09-30 16:25:39 -0400

    finished chapter 21

---
+++ week_9/chapter_19/StaticTableDemo/StaticTableDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StaticTableDemo
+//
+//  Created by Michael Metzger  on 9/30/15.
+//  Copyright © 2015 Michael Metzger . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
